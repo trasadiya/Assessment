635a5da45f97c3e734e3993e39a89ac022b86215
trasadiya <137395071+trasadiya@users.noreply.github.com>
2023-09-13 15:37:47 +0530

    Add files via upload

---
+++ JQuery Basic/assessment.js
@@ -0,0 +1,6 @@
+let mainNav = document.getElementById('d-show');
+let navBarToggle = document.getElementById('click');
+
+navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active');
+});
